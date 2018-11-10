0e6aed7d5b966082db5ae2e54d592d609a2ce10b
armasog <43734687+armasog@users.noreply.github.com>
2018-11-09 23:18:25 -0500

    Added parameters for Gambling Module

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 user = ''  # Enter your username here
 password = ''  # Enter your password here
 
-# Algorithm parameters
+# Auction algorithm parameters
 years = [18]  # Years of price history to consider. Entered as two digit integer format i.e 2018 is entered as 18
-months = ['Sep', 'Oct', 'Nov']  # Months of price history to consider. Entered in 3 letter
-minimum = 1000  # Minimum bid value for opportunities to display
-maximum = 100000  # Maximum bid value for opportunities to display
+months = ['Oct', 'Nov']  # Months of price history to consider. Entered in 3 letter
+minimum = 100  # Minimum bid value for opportunities to display
+maximum = 850000  # Maximum bid value for opportunities to display
+
+# Runic Aleatorem Bet Types
+circle_bet = 'https://www.hexrpg.com/ra/bet.php?circle=2'  # Arbitrarily selected circle 2
+color_bet = 'https://www.hexrpg.com/ra/bet.php?color=B'  # Arbitrarily selected black
+symbol_bet = 'https://www.hexrpg.com/ra/bet.php?sym=21'  # Arbitrarily selected symbol 21
+
+# Runic Aleatorem  Strategy Parameters
+iterations = 2  # Number of times to run strategy. Each run takes 300 seconds (5 minutes)
